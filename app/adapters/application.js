fa030b8a6b4ae32f71d7706bf0114e96d645453e
Tracy Lee <tracy@wingman.events>
2015-11-22 21:37:47 -0800

    marketing copy update

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,5 @@
 import EmberData from 'ember-data';
-import investors from '.com/utils/mentors';
+
 export default EmberData.JSONAPIAdapter.extend({
-	model: function() {
-		return mentors;
-		}
+	
 });
