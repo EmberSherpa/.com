ab6f67c3d537e1ac94fe9938d09d7caf918d5f0b
Tracy Lee <tracy@wingman.events>
2015-12-21 03:09:30 -0500

    saving marketing changes

--- app/utils/mentors.js
+++ app/utils/mentors.js
@@ -8,4 +8,24 @@ export default [{
 	image: 'images/sam.jpg',
 	cardContent: 'Sam is the creator of <a href="http://www.ember-cli-mirage.com/">EmberCLI Mirage</a>. Sam has extensive experience writing Ember.js applications using Ruby on Rails as backend.',
 	bookingLink: 'https://squareup.com/appointments/book/0badee89-09fd-43d2-b7eb-d9b2939e3a49/start?staff_id=1f2c76c6-f648-476e-9ce4-0ecd1ebccc0b' 
+},{
+	cardTitle: 'Sam Selikoff',
+	image: 'images/sam.jpg',
+	cardContent: 'Sam is the creator of <a href="http://www.ember-cli-mirage.com/">EmberCLI Mirage</a>. Sam has extensive experience writing Ember.js applications using Ruby on Rails as backend.',
+	bookingLink: 'https://squareup.com/appointments/book/0badee89-09fd-43d2-b7eb-d9b2939e3a49/start?staff_id=1f2c76c6-f648-476e-9ce4-0ecd1ebccc0b' 
+},{
+	cardTitle: 'Sam Selikoff',
+	image: 'images/sam.jpg',
+	cardContent: 'Sam is the creator of <a href="http://www.ember-cli-mirage.com/">EmberCLI Mirage</a>. Sam has extensive experience writing Ember.js applications using Ruby on Rails as backend.',
+	bookingLink: 'https://squareup.com/appointments/book/0badee89-09fd-43d2-b7eb-d9b2939e3a49/start?staff_id=1f2c76c6-f648-476e-9ce4-0ecd1ebccc0b' 
+},{
+	cardTitle: 'Sam Selikoff',
+	image: 'images/sam.jpg',
+	cardContent: 'Sam is the creator of <a href="http://www.ember-cli-mirage.com/">EmberCLI Mirage</a>. Sam has extensive experience writing Ember.js applications using Ruby on Rails as backend.',
+	bookingLink: 'https://squareup.com/appointments/book/0badee89-09fd-43d2-b7eb-d9b2939e3a49/start?staff_id=1f2c76c6-f648-476e-9ce4-0ecd1ebccc0b' 
+},{
+	cardTitle: 'Sam Selikoff',
+	image: 'images/sam.jpg',
+	cardContent: 'Sam is the creator of <a href="http://www.ember-cli-mirage.com/">EmberCLI Mirage</a>. Sam has extensive experience writing Ember.js applications using Ruby on Rails as backend.',
+	bookingLink: 'https://squareup.com/appointments/book/0badee89-09fd-43d2-b7eb-d9b2939e3a49/start?staff_id=1f2c76c6-f648-476e-9ce4-0ecd1ebccc0b' 
 }];
